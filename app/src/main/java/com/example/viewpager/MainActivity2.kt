9c0a9d8ab3049c8f6d1abc9a15cd437643551d53
Areeb <areebrockzz786@gmail.com>
2022-11-19 23:41:30 +0530

    First commit

--- app/src/main/java/com/example/viewpager/MainActivity2.kt
+++ app/src/main/java/com/example/viewpager/MainActivity2.kt
@@ -2,10 +2,12 @@ package com.example.viewpager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        Log.e("hello", "hello")
     }
 }
